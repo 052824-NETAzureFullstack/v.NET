d48112370f74a416c1fc53ee0dabcaa4a5a037a8
vonwe <vonwe@pvamu.edu>
2024-06-11 09:48:04 -0500

    Fixed Fizzbuzz functionality

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -18,14 +18,14 @@ namespace FizzBuzz
 
             for (int i = lower; i <= upper; i++)
             {
-                if (i % 3 == 0) {
-                    Console.WriteLine("Fizz");
+                if (i % 3 == 0 && i % 5 == 0) {
+                    Console.WriteLine("FizzBuzz");
                 }
                 else if (i % 5 == 0) {
                     Console.WriteLine("Buzz");
                 }
-                else if (i % 3 == 0 && i % 5 == 0) {
-                    Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0) {
+                    Console.WriteLine("Fizz");
                 }
                 else 
                     Console.WriteLine(i);
