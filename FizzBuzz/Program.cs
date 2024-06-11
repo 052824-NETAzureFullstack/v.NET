99080cbcd4692f235fefcc84530d1d502348f1fe
vonwe <vonwe@pvamu.edu>
2024-06-11 09:37:44 -0500

    Added FizzBuzz substitution

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Prompt user to input lower and upper values
+
+            Console.Write("Please enter a valid lower limit: ");
+            string userIn = Console.ReadLine();
+            int lower = Int32.Parse(userIn);
+
+            Console.Write("Please enter a valid upper limit: ");
+            userIn = Console.ReadLine();
+            int upper = Int32.Parse(userIn);
+
+            for (int i = lower; i <= upper; i++)
+            {
+                if (i % 3 == 0) {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0) {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0 && i % 5 == 0) {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else 
+                    Console.WriteLine(i);
+            }
+
+        }
+    }
+}
